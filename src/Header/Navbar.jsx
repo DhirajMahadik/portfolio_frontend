890c522d7e4a7f261fbc78c23d596759768cd177
Dhiraj Mahadik <dhirajmahadik9221@gmail.com>
2023-10-25 00:15:27 +0530

    Navigation bar updated

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -5,18 +5,18 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-transparent navbar-dark z-1 position-absolute w-100">
+            <nav className="navbar navbar-expand-lg  navbar-dark z-1 position-absolute w-100" style={{backgroundColor:'#00000080'}}>
                 <div className="container">
                     <Link className="navbar-brand fw-bold fst-italic fs-3" to="/">D<span className="text-danger">M</span></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse justify-content-end navbar-collapse bg-dark px-4 rounded" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <Link  className="nav-link text-light" aria-current="page" to="/">Home</Link>
-                            <a  className="nav-link text-light" href="#work-samples">Portfolio</a>
-                            <a  className="nav-link text-light" href="#skills">Skills</a>
-                            <a  className="nav-link text-light" href="#contact">Contact</a>
+                    <div className="collapse justify-content-end navbar-collapse  rounded" id="navbarNavAltMarkup">
+                        <div className="navbar-nav mx-2">
+                            <Link  className="nav-link text-light text-end" aria-current="page" to="/">Home</Link>
+                            <a  className="nav-link text-light text-end " href="#work-samples" >Portfolio</a>
+                            <a  className="nav-link text-light text-end" href="#skills">Skills</a>
+                            <a  className="nav-link text-light text-end" href="#contact">Contact</a>
                         </div>
                     </div>   
                 </div>
